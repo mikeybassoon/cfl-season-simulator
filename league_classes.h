b7c5d059d2f06298632038c2a8f9d25b1e4ddc00
Mike Macaulay <49072733+mikeybassoon@users.noreply.github.com>
2019-08-03 14:24:50 -0600

    Move contents of interface.h to league_classes.h

--- league_classes.h
+++ league_classes.h
@@ -9,6 +9,10 @@
 #define LEAGUE_CLASSES_H_
 
 #define NUMBER_OF_TEAMS 9
+#define MAX_GAMES_PER_WEEK 4 // maximum number of games possible in any one week
+#define NUMBER_OF_WEEKS 21 // weeks in season
+#define NUMBER_OF_SIMULATIONS 100 // number of simulations to run
+#define GAMES_PLAYED 18
 
 #include <iostream>
 #include <string>
@@ -20,18 +24,17 @@ private:
 	string name;
 	string division; // "west" or "east"
 	int teamID;
-	// win/loss/tie records against each team
-	// sorted by team ID
-	int winsAgainst[NUMBER_OF_TEAMS];
-	int lossesAgainst[NUMBER_OF_TEAMS];
-	int tiesAgainst[NUMBER_OF_TEAMS];
+	int winsAgainst[NUMBER_OF_TEAMS]; // wins against specific teams
+	int lossesAgainst[NUMBER_OF_TEAMS]; // losses against specific teams
+	int tiesAgainst[NUMBER_OF_TEAMS]; // ties against specific teams
 	int gamesPlayed; // in whole season
-	int pointsScoredAgainst[NUMBER_OF_TEAMS];
-	int pointsAllowedAgainst[NUMBER_OF_TEAMS];
+	int pointsScoredAgainst[NUMBER_OF_TEAMS]; // points scored against specific teams
+	int pointsAllowedAgainst[NUMBER_OF_TEAMS]; // points allowed against specific teams
+	int firstPlaceFinishes, secondPlaceFinishes, thirdPlaceFinishes, 
+		crossovers, timesMissedPlayoffs; // how many times has team had each final ranking result?
+	bool isRanked; // has team been assigned a final ranking for the season?
 public:
-	/*	Team
-	 *
-	 * 	Purpose: constructor
+	/*	Team constructor
 	 * 	Parameters:
 	 * 		<1> string - team name
 	 * 		<2> string - division
@@ -57,7 +60,39 @@ public:
 	double get_netQuotient() const; // returns net quotient of points against league
 	double get_netQuotient(int) const; // returns net quotient  of points against (teamID)
 	double get_netQuotient(string&) const; // returns net quotient of points against (teamID)
+	
+	// require get functions for season ranking results, isRanked
+	// require set functions for everything
 };
+
+struct GAME{
+	bool scheduled;
+	bool completed;
+	TEAM homeTeam;
+	TEAM awayTeam;
+	int week; // the week the game is scheduled in
+	int homeScore;
+	int awayScore;
+};
+
+// function prototypes from previous version of code - some or all of these may be redundant
+
+void assignTeamNames(TEAM[]);
+void createOutputFile();
+void crunchGameStats(TEAM[], GAME&);
+void crunchSeasonStats(TEAM[], GAME[NUMBER_OF_WEEKS][MAX_GAMES_PER_WEEK]);
+void copySchedule(GAME[NUMBER_OF_WEEKS][MAX_GAMES_PER_WEEK], const GAME[NUMBER_OF_WEEKS][MAX_GAMES_PER_WEEK]);
+void readSeasonSchedule(TEAM[], GAME[NUMBER_OF_WEEKS][MAX_GAMES_PER_WEEK]);
+void simulateGame(GAME&);
+void simulateSeason(TEAM[], GAME[][MAX_GAMES_PER_WEEK]);
+void simulateWeek(TEAM[], GAME[]);
+int getTeamId(string);
+void CalculateWinPercentage(TEAM[]);
+void CalculateTotals(TEAM[]);
+void CalculateNetAggregate(TEAM[]);
+void CalculateNetQuotient(TEAM[]);
+void sortDivision(TEAM[], TEAM[], TEAM[]);
+TEAM breakTie(TEAM, TEAM);
 
 
 
