2a09167b67385f7cecf6de97d72b5c5624ec7724
mikeybassoon <49072733+mikeybassoon@users.noreply.github.com>
2019-08-03 13:16:45 -0600

    Add class-based interface for teams
    Teams were previously declared as structs. league_classes.h and league_classes.cpp provide interface and implementation for Teams as classes. The new "Team" class includes member functions for calculating all of the playoff tiebreaker statistics.
    
    !! note that the implementation of the program itself has not been modified and at this time continues to use the struct implementation of teams

---
+++ league_classes.cpp
@@ -0,0 +1,152 @@
+/*
+ * league_classes.cpp
+ *
+ *  Created on: Jul. 30, 2019
+ *      Author: michael
+ */
+
+#include <iostream>
+#include "league_classes.h"
+#include <cassert>
+
+using namespace std;
+
+
+Team::Team(string teamName, string div, int id){
+	name = teamName;
+	division = div;
+	teamID = id;
+	for(int i = 0; i < LEAGUE_SIZE; i++){
+		winsAgainst[i] = 0;
+		lossesAgainst[i] = 0;
+		tiesAgainst[i] = 0;
+	}
+	gamesPlayed = 0;
+}
+
+string Team::get_name(){
+	return name;
+}
+
+string Team::get_division(){
+	return division;
+}
+
+int Team::get_playoffPoints() const{
+	return get_wins() * 2 + get_ties();
+}
+
+int Team::get_teamID(){
+	return teamID;
+}
+
+int Team::get_ties() const{
+	int totalTies = 0;
+	for(auto i:tiesAgainst)
+		totalTies += i;
+	return totalTies;
+}
+
+int Team::get_wins() const{
+	int totalWins = 0;
+	for(auto i:winsAgainst)
+		totalWins += i;
+	return totalWins;
+}
+
+int Team::get_winPercentage(int opponentID) const{
+	int winPct = (winsAgainst[opponentID] * 1000) /
+			(lossesAgainst[opponentID] + tiesAgainst[opponentID]);
+	return winPct;
+}
+
+int Team::get_winPercentage(string& division) const{
+	assert(division == "east" || division == "west");
+	int divisionWins = 0;
+	int divisionLosses = 0;
+	int divisionTies = 0;
+	if(division == "west"){
+		for(int x = 0; x <= 4; x++){
+			divisionWins += winsAgainst[x];
+			divisionLosses += lossesAgainst[x];
+			divisionTies += tiesAgainst[x];
+		}
+	}
+	else if(division == "east"){
+		for(int x = 5; x <= 8; x++){
+			divisionWins += winsAgainst[x];
+			divisionLosses += lossesAgainst[x];
+			divisionTies += tiesAgainst[x];
+		}
+	}
+
+	int divisionPct = (divisionWins * 1000) /
+			(divisionLosses + divisionTies);
+	return divisionPct;
+}
+
+int Team::get_netAggregate() const{
+	int netAgg = 0;
+	for(auto i:pointsScoredAgainst)
+		netAgg += i;
+	for(auto j:pointsAllowedAgainst)
+		netAgg -= j;
+	return netAgg;
+}
+
+int Team::get_netAggregate(int opponentID) const{
+	int netAgg = pointsScoredAgainst[opponentID] - pointsAllowedAgainst[opponentID];
+	return netAgg;
+}
+
+int Team::get_netAggregate(string& division) const{
+	assert(division == "east" || division == "west");
+	int netAgg = 0;
+	if(division == "east"){
+		for(int i = 5; i <= 8; i++){
+			netAgg += pointsScoredAgainst[i];
+			netAgg -= pointsAllowedAgainst[i];
+		}
+	}
+	else if(division == "west"){
+		for(int i = 0; i <= 4; i++){
+			netAgg += pointsScoredAgainst[i];
+			netAgg -= pointsAllowedAgainst[i];
+		}
+	}
+	return netAgg;
+}
+
+double Team::get_netQuotient() const{
+	double pointsFor;
+	double pointsAgainst;
+	for(auto i:pointsScoredAgainst) // points scored against all opponents
+		pointsFor += i;
+	for(auto j:pointsAllowedAgainst) // points allowed against all opponents
+		pointsAgainst += j;
+	return pointsFor / pointsAgainst;
+}
+
+double Team::get_netQuotient(int opponent) const{
+	return (double)pointsScoredAgainst[opponent] /
+			(double)pointsAllowedAgainst[opponent];
+}
+
+double Team::get_netQuotient(string& division) const{
+	assert(division == "east" || division == "west");
+	double pointsFor;
+	double pointsAgainst;
+	if(division == "east"){
+		for(int i = 5; i <= 8; i++){ // for all teams in east division
+			pointsFor += pointsScoredAgainst[i];
+			pointsAgainst += pointsAllowedAgainst[i];
+		}
+	}
+	else if(division == "west"){
+		for(int i = 0; i <=5; i++){ // for all teams in west division
+			pointsFor += pointsScoredAgainst[i];
+			pointsAgainst += pointsAllowedAgainst[i];
+		}
+	}
+	return pointsFor/pointsAgainst;
+}
---
+++ league_classes.h
@@ -0,0 +1,67 @@
+/*
+ * league_classes.h
+ *
+ *  Created on: Jul. 30, 2019
+ *      Author: michael
+ */
+
+#ifndef LEAGUE_CLASSES_H_
+#define LEAGUE_CLASSES_H_
+
+#define LEAGUE_SIZE 9
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Team{
+private:
+	string name;
+	string division; // "west" or "east"
+	int teamID;
+	// win/loss/tie records against each team
+	// sorted by team ID
+	int winsAgainst[LEAGUE_SIZE];
+	int lossesAgainst[LEAGUE_SIZE];
+	int tiesAgainst[LEAGUE_SIZE];
+	int gamesPlayed; // in whole season
+	int pointsScoredAgainst[LEAGUE_SIZE];
+	int pointsAllowedAgainst[LEAGUE_SIZE];
+public:
+	/*	Team
+	 *
+	 * 	Purpose: constructor
+	 * 	Parameters:
+	 * 		<1> string - team name
+	 * 		<2> string - division
+	 * 		<3> int - team id
+	 *	Preconditions: division is either "west" or "east"
+	 *		team id between 0 and LEAGUE_SIZE
+	 */
+	Team(string, string, int); // constructor
+	~Team(); // destructor
+	string get_name();
+	string get_division();
+	int get_teamID();
+
+	// playoff tiebreaker criteria
+	int get_playoffPoints() const;
+	int get_ties() const;
+	int get_wins() const; // wins against all teams
+	int get_winPercentage(int) const; // returns win percentage against(teamID)
+	int get_winPercentage(string&) const; // returns win percentage against ("division")
+	int get_netAggregate() const; // returns net aggregate of points against league
+	int get_netAggregate(int) const; // returns net aggregate of points against (teamID)
+	int get_netAggregate(string&) const; // returns net aggregate of points against ("division)
+	double get_netQuotient() const; // returns net quotient of points against league
+	double get_netQuotient(int) const; // returns net quotient  of points against (teamID)
+	double get_netQuotient(string&) const; // returns net quotient of points against (teamID)
+};
+
+
+
+
+
+#endif /* LEAGUE_CLASSES_H_ */
+
