0a8e65a582bc0d1d568398832acf55a7f40b6e03
mikeybassoon <mike.macaulay@gmail.com>
2019-12-21 00:22:09 -0600

    Confirm successful initialization of league class

--- javascript/league.js
+++ javascript/league.js
@@ -9,6 +9,11 @@ const GAMES_IN_SEASON = 18;
 const CFL = {
 	team: [ 'CGY', 'EDM', 'SSK', 'BC', 'WPG', 'HAM', 'OTT', 'TOR', 'MTL' ],
 	schedule: [] // each unit in the schedule is an array of games for a particular week	
+	
+	// member functions
+	// initialize - set wins and losses to zero
+	// get and set functions for statistics
+	
 };
 
 // print out contents of team array to test
