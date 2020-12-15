7e63418b9ea5a00462263ed240413f9282a6c087
Mike Macaulay <49072733+mikeybassoon@users.noreply.github.com>
2020-12-15 15:39:22 -0600

    Start Python rework of calculator

---
+++ python/stats_engine/cfl_playoff_functions.py
@@ -0,0 +1,32 @@
+"""
+TIEBREAKER SUMMARY (found at cfldb.ca, an invaluable resource)
+
+When two or more member Clubs are tied in the final Division standings at the conclusion of the regular season schedule, preferential ranking for playoff purposes shall be determined on the basis of the following descending-order priorities and shall be awarded to the club that:
+
+    has won the greater number of games played against all member Clubs of the League, then
+    has the higher winning percentage in all games played against all the other tied Club(s), then
+    has scored the higher net aggregate of points (i.e. points scored for less points scored against) in games played against all the other tied Club(s), then
+    has scored the higher net quotient of points (i.e. points scored for divided by points scored against) in games played against all the other tied Clubs(s), then
+    has the higher winning percentage in all games played against all member Clubs of the Division, then
+    has scored the higher net aggregate of points in games played against all member Clubs of the Division, then
+    has scored the higher net quotient of points in games played against all member Clubs of the Division, then
+    has scored the higher net aggregate of points in games played against all member Clubs of the League, then
+    has scored the higher net quotient of points in games played against all member Clubs of the League, then
+    has won a coin toss against the other tied club.
+
+Note: If two Clubs remain tied after other Club(s) are eliminated during any step, tie breaker reverts to step a).
+"""
+
+# function to rank a list of clubs by number of games won
+
+# function to rank a list of clubs by highest winning percentage against each other
+
+# function to rank a list of clubs by total net aggregate (points scored less points allowed) against each other
+
+# function to rank a list of clubs by total net quotient (points scored divided by points allowed) against each other
+
+# function to rank a list of clubs by highest net aggregate points against (either their division or the full league)
+
+# function to rank a list of clubs by highest net quotient points against (either their division or the full league)
+
+# helper function for the two functions given scope
---
+++ python/tests/index.py
@@ -0,0 +1,11 @@
+# index.py
+#
+# Runs all tests, outputs number of tests run and passed
+
+# initialize test counters to zero
+testsPassed = 0
+testsRun = 0
+
+
+# output final test counts
+print(str(testsPassed) + " tests passed out of " + str(testsRun))
